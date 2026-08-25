d8b015cafcca82152b084ad3018c90c95d935511
GokulRaj <gokulrajprabu2000@gmail.com>
2026-08-25 16:35:54 +0530

    Add files via upload

---
+++ first task.py
@@ -0,0 +1,92 @@
+a = int(input())
+b = int (input())
+c= int(input())
+add =(a+b)
+sub =(a-b)
+mul=(a*b)
+div =(a/b)
+mod=(a//b)
+power=(a**2)
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(power)
+radius= a/2
+
+area_c =(3.14*radius)**2
+print (area_c)
+area_rec=2*(a*b)
+print(area_rec)
+rectangle_per=2*(a+b)
+print(rectangle_per)
+area_sq=(a**2)
+print(area_sq)
+sq_per=(4*a)
+print(sq_per)
+circle=(2*3.14*radius)
+print(circle)
+avg =(a+b+c)/3
+print(avg)
+
+if(a==b):
+    print("true")
+    
+else:
+    print("false")
+
+if(a>b):
+    print("true")
+else:
+    print("false")
+if(a<=b):
+    print("true")
+else:
+    print("false")
+    
+square_root=a**0.5
+print(square_root)
+cube_root=a**1.5
+print(cube_root)
+
+
+p=int(input("amount"))
+n= int(input("year"))
+t = int (input("time"))
+r=int(input())
+si =((p*n*t)/100)
+print(si)
+ci=p*(1+r/n)**n*t
+print(ci)
+
+x=10
+print(x)
+x+=5
+print(x)
+x-=3
+print(x)
+x/=4
+print(x)
+x%=2
+print(x)
+x**=3
+print(x)
+
+a=(a+b)
+print(a)
+b=(a-b)
+print(b)
+a=(a-b)
+print(a)
+
+name="Raj"
+pass_word="1234"
+enter_name=input("enter name")
+enter_password=input("enter password")
+if name==enter_name and pass_word==enter_password:
+    print("correct")
+elif name !=enter_name or pass_word!=enter_password:
+        print("incorrect")
+
+
