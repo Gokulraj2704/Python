b66c532f8a588eef63dd69ac17b69e0eba41df91
GokulRaj <gokulrajprabu2000@gmail.com>
2026-09-02 16:24:37 +0530

    Add files via upload

---
+++ stringtask.py
@@ -0,0 +1,52 @@
+
+#q1
+a=str(input("enter the name"))
+print(a.upper())
+
+#q2
+a=str(input("enter the sentence"))
+print(a.lower())
+
+#q3andq8
+a=str(input("enter the sentence"))
+print(a.capitalize())
+print(a.expandtabs(tabsize=8))
+
+#q6
+a=str(input("enter"))
+print(a.find("a"))
+
+#q4
+a=str(input("enter the name"))
+b=str(input("enter the age"))
+print(f"I am {a},my age {b}")
+
+#q5
+a=str(input("enter"))
+print(a.index("a"))
+
+#q7
+a=str(input("enter"))
+print(a.endswith("ing"))
+
+#q10
+
+a=str(input("enter"))
+print(a.isdigit())
+
+#q11
+a=str(input("enter"))
+print(a.isnumeric())
+
+#q12
+a=str(input("enter"))
+print(a.isalnum())
+
+#q13
+a=str(input("enter"))
+print(a.isascii())
+
+#q14
+a=str(input("enter"))
+print(a.isalpha())
+
